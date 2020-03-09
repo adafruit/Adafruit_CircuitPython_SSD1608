e393e9cdad717cce4c61895cef8d5679d08c4bab
dherrada <dylan.herrada@gmail.com>
2020-03-09 19:31:48 -0400

    Ran black, updated to pylint 2.x

--- examples/ssd1608_simpletest.py
+++ examples/ssd1608_simpletest.py
@@ -13,17 +13,20 @@ import adafruit_ssd1608
 displayio.release_displays()
 
 # This pinout works on a Feather M4 and may need to be altered for other boards.
-spi = board.SPI() # Uses SCK and MOSI
+spi = board.SPI()  # Uses SCK and MOSI
 epd_cs = board.D9
 epd_dc = board.D10
 epd_reset = board.D5
 epd_busy = board.D6
 
-display_bus = displayio.FourWire(spi, command=epd_dc, chip_select=epd_cs, reset=epd_reset,
-                                 baudrate=1000000)
+display_bus = displayio.FourWire(
+    spi, command=epd_dc, chip_select=epd_cs, reset=epd_reset, baudrate=1000000
+)
 time.sleep(1)
 
-display = adafruit_ssd1608.SSD1608(display_bus, width=200, height=200, busy_pin=epd_busy)
+display = adafruit_ssd1608.SSD1608(
+    display_bus, width=200, height=200, busy_pin=epd_busy
+)
 
 g = displayio.Group()
 
